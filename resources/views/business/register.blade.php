ce64a0ca9124f7ce7f5ba2f1b695fdd821d9ee7f
Ian Vinales <ian.vinales26@gmail.com>
2020-07-12 00:48:09 -0400

    fixed google maps

--- resources/views/business/register.blade.php
+++ resources/views/business/register.blade.php
@@ -134,6 +134,8 @@
 
 @section('scripts')
     @parent
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/main.js') }}" defer></script>
     <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&libraries=places&callback=initialize" async defer></script>
     <script src="/js/mapInput.js"></script>
 @stop
